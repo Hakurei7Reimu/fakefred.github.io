94c0e83e23ac2fd8a6f4b0d8559a6994a8c4430f
Frederick <fractionfred@gmail.com>
2017-02-25 07:23:29 -0600

    Add files via upload

---
+++ script/calc.js
@@ -0,0 +1,26 @@
+function plus(){
+	var a = prompt("数值1=");
+	var b = prompt("数值2=");
+	for(var r = 0; r<b; r++){
+		a++;
+	}
+	alert(a);
+}
+
+function minus(){
+	var a = prompt("数值1=");
+	var b = prompt("数值2=");
+	alert(a-b);
+}
+
+function multiply(){
+	var a = prompt("数值1=");
+	var b = prompt("数值2=");
+	alert(a*b);
+}
+
+function divide(){
+	var a = prompt("数值1=");
+	var b = prompt("数值2=");
+	alert(a/b);
+}
