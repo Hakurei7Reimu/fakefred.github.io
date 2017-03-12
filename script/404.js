005ce5db66ec5d78f4e8d872ad311858f34fd847
fakefred <fractionfred@gmail.com>
2017-03-12 12:58:56 +0800

    update 404.js: add 404 messages and links.

--- script/404.js
+++ script/404.js
@@ -4,33 +4,33 @@ var rand = Math.floor(randNotFloored);
 
 switch(rand){
 	case 0:
-		elem404.innerHTML="这个网站目前还是假的，无法访问";
+		elem404.innerHTML="这个网站目前还是假的，无法访问<br/>试试<a href=\"trade.html\">LINK</a>";
 	break;
 	case 1:
-		elem404.innerHTML="这个网站还在开车，暂时就不来了";
+		elem404.innerHTML="这个网站还在开车，暂时就不来了<br/>试试<a href=\"function.html\">LINK</a>";
 	break;
 	case 2:
-		elem404.innerHTML="这个网站正在秋名山旅游，还没有回来";
+		elem404.innerHTML="这个网站正在秋名山旅游，还没有回来<br/>试试<a href=\"quad.html\">LINK</a>";
 	break;
 	case 3:
-		elem404.innerHTML="这个网站正在搞比利，等一会儿再搞你";
+		elem404.innerHTML="这个网站正在搞比利，等一会儿再搞你<br/>试试<a href=\"ads.html\">LINK</a>";
 	break;
 	case 4:
-		elem404.innerHTML="我是谁？我在哪儿？";
+		elem404.innerHTML="我是谁？我在哪儿？<br/>试试<a href=\"pic.html\">LINK</a>";
 	break;
 	case 5:
-		elem404.innerHTML="您的属性 节操 值为0，请充值后访问";
+		elem404.innerHTML="您的属性 节操 值为0，请充值后访问<br/>试试<a href=\"https://github.com/fakefred/fakefred.github.io/\">LINK</a>";
 	break;
 	case 6:
-		elem404.innerHTML="不要问我404是什么意思，它其实相当于233";
+		elem404.innerHTML="不要问我404是什么意思<br/>试试<a href=\"https://www.coolapk.com/u/587254/\">LINK</a>";
 	break;
 	case 7:
-		elem404.innerHTML="这个网站正在和boy♂next♂door玩耍，不鸟你";
+		elem404.innerHTML="这个网站正在和boy♂next♂door玩耍，不鸟你<br/>试试<a href=\"https://t.me/fakefred\">LINK</a>";
 	break;
 	case 8:
-		elem404.innerHTML="";
+		elem404.innerHTML="此网站水表坏了，正在抢修";
 	break;
 	case 9:
-		elem404.innerHTML="";
+		elem404.innerHTML="根据相关法律法规，本内容暂时不予显示";
 	break;
 }
